aab72b598946e38c6bca17f1e63daa73ebd6ef63
Moiz459 <MoizSaleem903@gmail.com>
2024-02-24 13:33:50 +0000

    Projects Component Complete

---
+++ src/components/Projects.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import proj1 from '../assets/placeholder.png'
+
+const Projects = () => {
+  return (
+    <div class="py-6 max-w-[1200px] mx-auto">
+      <div class='mx-auto px-4 md:px-8'> 
+        <div class='mb-4 flex items-center justify-between'>
+          <div class='flex flex-col gap-4'>
+            <h2 class ="text-2xl lg:text-3xl text-white"> 
+              My <span>Projects</span>
+            </h2>
+            <p class='text-gray-800'>
+              There are my different projects
+            </p>
+
+           
+
+          </div>
+          
+
+        </div>
+        <div class='grid gap-4 grid-cols-2 sm:grid-cols-3 md:gap-6 xl:gap-8'>
+          
+          <a  class="group h-48 overflow-hidden rounded-lg shadow-lg md:h-80">
+            <img src={proj1} alt="" class="h-full w-full object-cover object-center transition
+                                            duration-200 group-hover:scale-110"/>
+
+          </a>
+
+          <a  class="group h-48 overflow-hidden rounded-lg shadow-lg md: col-span-2 md:h-80">
+            <img src={proj1} alt="" class="h-full w-full object-cover object-center transition
+                                            duration-200 group-hover:scale-110"/>
+
+          </a>
+
+          <a class="group h-48 overflow-hidden rounded-lg shadow-lg md: col-span-2 md:h-80">
+            <img src={proj1} alt="" class="h-full w-full object-cover object-center transition
+                                            duration-200 group-hover:scale-110"/>
+
+          </a>
+
+          <a  class="group h-48 overflow-hidden rounded-lg shadow-lg md:h-80">
+            <img src={proj1} alt="" class="h-full w-full object-cover object-center transition
+                                            duration-200 group-hover:scale-110"/>
+
+          </a>
+
+        </div>
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default Projects
